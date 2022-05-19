3acccab698b24b8ab1afd02b541284ba7ebe1b6c
bagusn13 <bagusn1367@gmail.com>
2022-05-19 21:44:09 +0700

    update packages

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
-const AdminJS = require("adminjs");
-const AdminJSExpress = require("@adminjs/express");
-const AdminJSSequelize = require("@adminjs/sequelize");
+
 const db = require("./models");
 const express = require("express");
 const log = require("./logger");
@@ -13,37 +11,6 @@ app.use(compression());
 
 const routes = require("./routes");
 const port = process.env.PORT_APP || 5000;
-
-// // START ADMIN JS
-// AdminJS.registerAdapter(AdminJSSequelize);
-// const adminJs = new AdminJS({
-//   databases: [db],
-//   rootPath: "/admin",
-//   branding: {
-//     companyName: "TempatCukur.id",
-//   },
-// });
-
-// const ADMIN = {
-//   email: process.env.ADMIN_EMAIL || "noreply.tempatcukur@gmail.com",
-//   password: process.env.ADMIN_PASSWORD || "CukurSukses99",
-// };
-
-// const router = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
-//   cookieName: process.env.ADMIN_COOKIE_NAME || "adminjs",
-//   cookiePassword:
-//     process.env.ADMIN_COOKIE_PASS ||
-//     "supersecret-and-long-password-for-a-browser",
-//   authenticate: async (email, password) => {
-//     if (email === ADMIN.email && password === ADMIN.password) {
-//       return ADMIN;
-//     }
-//     return null;
-//   },
-// });
-
-// app.use(adminJs.options.rootPath, router);
-// // END ADMIN JS
 
 app.use(
   bodyParser.urlencoded({
